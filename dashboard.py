fae82a218f96fc2517ee3d94d55eed13b0f0967e
Robert Futch <robertsfutch@gmail.com>
2023-09-18 21:07:49 -0700

    Function to add new users

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-from ttkthemes import ThemedStyle  # Import ThemedStyle from ttkthemes
+from ttkthemes import ThemedStyle
+import csv
+import os
 
 import inventory_table
 import patients_table
@@ -11,13 +13,16 @@ import users_table
 inventory_tree = None
 patients_tree = None
 users_tree = None
-frame = None  # Define 'frame' as a global variable
-add_user_button = None  # Define 'add_user_button' as a global variable
+frame = None
+add_user_button = None
 
-# Function to create the dashboard window with the Inventory, Patients, and Users tables
+# Function to open the new user window
+def open_new_user_window():
+    os.system('python new_user_window.py')
+
+# Create the dashboard window
 def create_dashboard():
-    global inventory_tree, patients_tree, users_tree, frame, add_user_button  # Access the global variables
-    # Create the dashboard window
+    global inventory_tree, patients_tree, users_tree, frame, add_user_button
     dashboard = tk.Tk()
     dashboard.title("Dashboard")
 
@@ -26,82 +31,67 @@ def create_dashboard():
     dashboard.geometry("800x600")
     dashboard.resizable(False, False)
 
-    # Create a ThemedStyle instance for the modern theme
     style = ThemedStyle(dashboard)
-    style.set_theme("equilux")  # Use the "equilux" theme or choose another theme
+    style.set_theme("equilux")
 
-    # Create a frame to hold the content
     frame = ttk.Frame(dashboard)
     frame.pack(expand=True, fill="both")
 
-    # Create a horizontal frame for buttons
     button_frame = ttk.Frame(frame)
     button_frame.pack(side="top", fill="x", padx=10, pady=10)
 
-    # Create buttons for Inventory, Patients, Users, Settings, and Exit
     inventory_button = ttk.Button(button_frame, text="Inventory", command=show_inventory_table)
     patients_button = ttk.Button(button_frame, text="Patients", command=show_patients_table)
     users_button = ttk.Button(button_frame, text="Users", command=show_users_table)
     settings_button = ttk.Button(button_frame, text="Settings", command=show_user_button)
     exit_button = ttk.Button(frame, text="Exit", command=dashboard.quit)
 
-    # Pack buttons horizontally with padding
     inventory_button.pack(side="left", padx=10)
     patients_button.pack(side="left", padx=10)
     users_button.pack(side="left", padx=10)
     settings_button.pack(side="left", padx=10)
 
-    # Call the functions from the separate files to create the trees
     inventory_tree = inventory_table.create_inventory_table(frame)
     patients_tree = patients_table.create_patients_table(frame)
     users_tree = users_table.create_users_table(frame)
 
-    # Create an Exit button at the bottom right
     exit_button.pack(side="bottom", anchor="se", padx=10, pady=10)
 
-    # Start the Tkinter main loop for the dashboard
     dashboard.mainloop()
 
-# Function to show the Inventory table
 def show_inventory_table():
     inventory_table.show_inventory_table(inventory_tree)
     patients_table.hide_patients_table(patients_tree)
     users_table.hide_users_table(users_tree)
     hide_add_user_button()
 
-# Function to show the Patients table
 def show_patients_table():
     inventory_table.hide_inventory_table(inventory_tree)
     patients_table.show_patients_table(patients_tree)
     users_table.hide_users_table(users_tree)
     hide_add_user_button()
 
-# Function to show the Users table
 def show_users_table():
     inventory_table.hide_inventory_table(inventory_tree)
     patients_table.hide_patients_table(patients_tree)
     users_table.show_users_table(users_tree)
     hide_add_user_button()
 
-# Function to show settings buttons and hide the tables
 def show_user_button():
-    # Hide the table views
     inventory_table.hide_inventory_table(inventory_tree)
     patients_table.hide_patients_table(patients_tree)
     users_table.hide_users_table(users_tree)
     show_add_user_button()
 
-# Function to hide the "Add User" button
 def hide_add_user_button():
     global add_user_button
     if add_user_button is not None:
         add_user_button.pack_forget()
 
-# Function to show the "Add User" button
 def show_add_user_button():
     global add_user_button
     if add_user_button is None:
-        add_user_button = ttk.Button(frame, text="Add User")
+        add_user_button = ttk.Button(frame, text="Add User", command=open_new_user_window)
     add_user_button.pack(side="top", pady=10)
 
 if __name__ == "__main__":
---
+++ new_user_window.py
@@ -0,0 +1,69 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+import csv
+from ttkthemes import ThemedStyle  # Import ThemedStyle from ttkthemes
+
+# Function to handle the submit button click event
+def submit_user():
+    # Get the values from the entry widgets
+    username = username_entry.get()
+    password = password_entry.get()
+
+    # Validate the values
+    if not username or not password:
+        messagebox.showerror("Error", "Please enter both username and password")
+    else:
+        # Append the values to the CSV file with a newline character
+        with open("credentials.csv", "a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([username, password])
+
+        # Show a success message
+        messagebox.showinfo("Success", "New user added successfully")
+
+        # Clear the entry widgets
+        username_entry.delete(0, tk.END)
+        password_entry.delete(0, tk.END)
+
+# Create the new user window
+new_user_window = tk.Tk()
+new_user_window.title("New User")
+
+# Configure the window to have no border and make it resizable
+new_user_window.overrideredirect(True)
+new_user_window.geometry("400x200")
+new_user_window.resizable(False, False)
+
+# Create a ThemedStyle instance for the modern theme
+style = ThemedStyle(new_user_window)
+style.set_theme("equilux")  # Use the "equilux" theme or choose another theme
+
+# Create a frame to hold the content
+frame = ttk.Frame(new_user_window)
+frame.pack(expand=True, fill="both")
+
+# Create a label and entry widgets for username and password
+username_label = ttk.Label(frame, text="Username:")
+username_entry = ttk.Entry(frame)
+password_label = ttk.Label(frame, text="Password:")
+password_entry = ttk.Entry(frame, show="*")
+
+# Create a submit button
+submit_button = ttk.Button(frame, text="Submit", command=submit_user)
+
+# Use grid layout to arrange the widgets
+username_label.grid(row=0, column=0, padx=10, pady=5, sticky="e")
+username_entry.grid(row=0, column=1, padx=10, pady=5, columnspan=2, sticky="w")
+password_label.grid(row=1, column=0, padx=10, pady=5, sticky="e")
+password_entry.grid(row=1, column=1, padx=10, pady=5, columnspan=2, sticky="w")
+submit_button.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
+
+# Center the entry widgets in the window
+frame.grid_rowconfigure(0, weight=1)
+frame.grid_rowconfigure(1, weight=1)
+frame.grid_columnconfigure(0, weight=1)
+frame.grid_columnconfigure(2, weight=1)
+
+# Start the Tkinter main loop for the new user window
+new_user_window.mainloop()
