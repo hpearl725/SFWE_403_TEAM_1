599ddc4408bab62e00277c9e7a3d58f9d6793e60
Harrison Pearl <harrisonpearl@Harrisons-MacBook-Pro-2.local>
2023-10-03 11:55:38 -0700

    using os.path instead of hard code file access, mac user problem

--- GUI/inventory_table.py
+++ GUI/inventory_table.py
@@ -1,7 +1,9 @@
+import os
 import tkinter as tk
 from tkinter import ttk
 import csv
 from GUI.inventory import read_inventory
+
 
 def create_inventory_table(frame):
     inventory_tree = ttk.Treeview(frame)
@@ -19,10 +21,13 @@ def create_inventory_table(frame):
 
     return inventory_tree
 
-def show_inventory_table(inventory_tree):
-    inventory_tree.delete(*inventory_tree.get_children())  # Clear existing rows
 
-    inventory_dict = read_inventory("GUI/inventory.csv")
+def show_inventory_table(inventory_tree):
+    inventory_tree.delete(*inventory_tree.get_children()
+                          )  # Clear existing rows
+
+    inventory_path = os.path.join('GUI', 'inventory.csv')
+    inventory_dict = read_inventory(inventory_path)
 
     for row in inventory_dict.values():
         name = row["product_name"]
@@ -31,6 +36,7 @@ def show_inventory_table(inventory_tree):
         inventory_tree.insert("", tk.END, values=(name, quantity, price))
 
     inventory_tree.pack()
+
 
 def hide_inventory_table(inventory_tree):
     inventory_tree.pack_forget()
