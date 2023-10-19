b7ad7d0f8754df205269c13e7b2db392af0d9e2a
Harrison Pearl <harrison725@icloud.com>
2023-10-19 00:14:47 -0700

    created new window and changed functionality to create new user, now requires more info to create user

--- GUI/new_user_window.py
+++ GUI/new_user_window.py
@@ -27,7 +27,7 @@ def generate_password(length):
 
 # Function to handle the submit button click event
 # This function submit_user is part of this module.
-def submit_user(username_entry,window):
+def submit_user(user_entry,window):
     """
     This function handles the submit button click event.
 
@@ -35,15 +35,14 @@ def submit_user(username_entry,window):
     appends the values to the CSV file, shows a success message, and destroys the window.
     """
     # Get the values from the entry widgets
-    username = username_entry.get()
-
+    row =  [get_max_id()+1] + [x[1].get() for x in user_entry] +  [0,'True', 'False']
     # Generate a random password
     password = generate_password(10)
-
+    row.insert(2, password)
     # Append the values to the CSV file with a newline character
-    with open(os.path.join("GUI","credentials.csv"), "a", newline="") as file:
+    with open(os.path.join("GUI","users.csv"), "a", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow([username, password, True, "staff"])
+        writer.writerow(row)
 
     # Show a success message and destroy the window
     messagebox.showinfo("Success", f"New user added successfully. The temporary password is {password}")
@@ -59,7 +58,7 @@ def create_new_user_window():
 
     # Configure the window to have no border and make it resizable
     new_user_window.overrideredirect(True)
-    new_user_window.geometry("400x200")
+    new_user_window.geometry("400x400")  # Increased height to accommodate additional fields
     new_user_window.resizable(False, False)
 
     # Create a ThemedStyle instance for the modern theme
@@ -70,23 +69,50 @@ def create_new_user_window():
     frame = ttk.Frame(new_user_window)
     frame.pack(expand=True, fill="both")
 
-    # Create a label and entry widgets for username and password
-    username_label = ttk.Label(frame, text="Username:")
-    username_entry = ttk.Entry(frame)
+    # Create labels and entry widgets for additional fields
+    # need to get maxid from users.csv
+    fields = [
+        ("Username:", ttk.Entry(frame)),
+        ("Role:", ttk.Entry(frame)),
+        ("First Name:", ttk.Entry(frame)),
+        ("Last Name:", ttk.Entry(frame)),
+        ("Date of Birth:", ttk.Entry(frame)),
+        ("Phone Number:", ttk.Entry(frame)),
+        ("Email Address:", ttk.Entry(frame))
+    ]
 
     # Create a submit button
-    submit_button = ttk.Button(frame, text="Submit", command=lambda: submit_user(username_entry,new_user_window))
+    submit_button = ttk.Button(frame, text="Submit", command=lambda: submit_user(fields, new_user_window))
 
     # Use grid layout to arrange the widgets
-    username_label.grid(row=0, column=0, padx=10, pady=5, sticky="e")
-    username_entry.grid(row=0, column=1, padx=10, pady=5, columnspan=2, sticky="w")
-    submit_button.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
+    for i, (label_text, entry_widget) in enumerate(fields):
+        label = ttk.Label(frame, text=label_text)
+        label.grid(row=i, column=0, padx=10, pady=5, sticky="e")
+        entry_widget.grid(row=i, column=1, padx=10, pady=5, columnspan=2, sticky="w")
+
+    submit_button.grid(row=len(fields), column=0, columnspan=3, padx=10, pady=10)
 
     # Center the entry widgets in the window
-    frame.grid_rowconfigure(0, weight=1)
-    frame.grid_rowconfigure(1, weight=1)
+    for i in range(len(fields) + 1):
+        frame.grid_rowconfigure(i, weight=1)
     frame.grid_columnconfigure(0, weight=1)
     frame.grid_columnconfigure(2, weight=1)
 
     # Start the Tkinter main loop for the new user window
     new_user_window.mainloop()
+
+def get_max_id():
+    try:
+        with open(os.path.join("GUI","users.csv"), 'r', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            # Skip the header row if it exists
+            next(reader, None)
+            # Count the number of entries in the CSV file
+            num_entries = sum(1 for row in reader)
+            return num_entries
+    except FileNotFoundError:
+        print("The 'users.csv' file does not exist.")
+        return 0
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return 0
